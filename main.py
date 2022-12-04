513cdfcb9e027d6ceb9be09f7f2e9e43c5251a6b
ssieza <sikimiteeliost.sieza@epfedu.fr>
2022-12-04 18:28:25 +0100

    Update code

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-def app(event):
- prefix_google = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-8XGX24827F"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-8XGX24827F');
-</script>
- """
- return prefix_google + "Hello World"
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
-def app(event):
-    return "Hello, world!"
+def hello_world():
+ prefix_google = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-B42PCT518X"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-B42PCT518X');
+</script>
+ """
+ return prefix_google + "Hello World"
