4d08005daf4c2e14bf679e602cde61e97f804e0d
ssieza <sikimiteeliost.sieza@epfedu.fr>
2023-01-03 20:16:02 +0100

    Update for lab 2

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+import requests
 
 app = Flask(__name__)
 
@@ -15,4 +17,32 @@ def hello_world():
   gtag('config', 'G-B42PCT518X');
 </script>
  """
- return prefix_google + "Hello World"
+ return prefix_google + "Hello World, i am Elioste, data science student at EPF engineering school"
+
+# Lab 2 : Test the python logger
+# Define response for request
+def redirect_response():
+    if request.form["submit"] == "Logger":
+        return redirect(url_for("logger"))
+    return "Connecté!"
+
+# Define logger on deta
+@app.route('/logger')
+@app.route('/logger', methods=["GET"])
+def logger():
+    print('Back-end logs :', file=sys.stderr)
+    logging.info("Logging test")
+    script = """
+    <script> console.log("Login page") </script>
+    """
+    return render_template("logger.html") + script
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+# Lab 2: Maniplate cookies - Request with oauth
+@app.route('/cookies', methods = ['GET', 'POST'])
+def get_cookies():
+    req = requests.get("https://www.google.com/")
+
+    return req.cookies.get_dict()
